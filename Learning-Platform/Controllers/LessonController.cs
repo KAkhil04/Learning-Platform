90342c548221c427878d9fa664faaeb1ab21655c
ajithreddybaddam <ajithreddyaj17@gmail.com>
2023-04-22 19:52:25 -0500

    Added controllers and handlers

---
+++ Learning-Platform/Controllers/LessonController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using LearningPlatform.Models;
+
+namespace LearningPlatform.Controllers
+{
+    public class LessonController : Controller
+    {
+        private readonly TrainingProgDbContext _context;
+
+        public LessonController(TrainingProgDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Lesson
+        public async Task<IActionResult> Index()
+        {
+              return _context.Lessons != null ? 
+                          View(await _context.Lessons.ToListAsync()) :
+                          Problem("Entity set 'TrainingProgDbContext.Lessons'  is null.");
+        }
+
+        // GET: Lesson/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Lessons == null)
+            {
+                return NotFound();
+            }
+
+            var lesson = await _context.Lessons
+                .FirstOrDefaultAsync(m => m.lesson_id == id);
+            if (lesson == null)
+            {
+                return NotFound();
+            }
+
+            return View(lesson);
+        }
+
+        // GET: Lesson/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Lesson/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("lesson_id,lesson_name,course_id,lesson_content,lesson_duration")] Lesson lesson)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(lesson);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(lesson);
+        }
+
+        // GET: Lesson/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Lessons == null)
+            {
+                return NotFound();
+            }
+
+            var lesson = await _context.Lessons.FindAsync(id);
+            if (lesson == null)
+            {
+                return NotFound();
+            }
+            return View(lesson);
+        }
+
+        // POST: Lesson/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("lesson_id,lesson_name,course_id,lesson_content,lesson_duration")] Lesson lesson)
+        {
+            if (id != lesson.lesson_id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(lesson);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LessonExists(lesson.lesson_id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(lesson);
+        }
+
+        // GET: Lesson/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Lessons == null)
+            {
+                return NotFound();
+            }
+
+            var lesson = await _context.Lessons
+                .FirstOrDefaultAsync(m => m.lesson_id == id);
+            if (lesson == null)
+            {
+                return NotFound();
+            }
+
+            return View(lesson);
+        }
+
+        // POST: Lesson/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Lessons == null)
+            {
+                return Problem("Entity set 'TrainingProgDbContext.Lessons'  is null.");
+            }
+            var lesson = await _context.Lessons.FindAsync(id);
+            if (lesson != null)
+            {
+                _context.Lessons.Remove(lesson);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LessonExists(int id)
+        {
+          return (_context.Lessons?.Any(e => e.lesson_id == id)).GetValueOrDefault();
+        }
+    }
+}
